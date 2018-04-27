0da11dc2442a5ec5cfae8db8d4d8e36df49b96c9
Jan Schmutz <jan.schmutz@arcor.de>
2018-04-27 13:05:39 +0200

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,22 @@
+const Posts = [
+    {
+        id: '0176413761b289e6d64c2c14a758c1c7',
+        author_id: '0hy894hf0dlkfh9oinv',
+        title: 'How to Eat',
+        body: 'This is some randomly generated Text. Welcome to GraphQl'
+    },
+    {
+        id: '0be4bea0330ccb5ecf781a9f69a64bc8',
+        author_id: '8dlx7ak38fd39dv79ad',
+        title: 'How to Sleep',
+        body: 'This is some randomly generated Text. Welcome to GraphQl'
+    },
+    {
+        id: '2f6b59fd0b182dc6e2f0051696c70d70',
+        author_id: '8dlx7ak38fd39dv79ad',
+        title: 'How to Rave',
+        body: 'This is some randomly generated Text. Welcome to GraphQl'
+    }
+];
+
+module.exports = Posts;
